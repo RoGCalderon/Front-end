71061574abd8f1f52e6bc88f7a7260ec4be388ec
RoGCalderon <guadac0516@gmail.com>
2022-07-20 02:45:21 -0300

    Actualización de componentes

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { ExperienciaLaboralComponent } from './components/experiencia-laboral/experiencia-laboral.component';
+import { HardSoftSkillsComponent } from './components/hard-soft-skills/hard-soft-skills.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { IdiomasComponent } from './components/idiomas/idiomas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AcercaDeComponent,
+    EducacionComponent,
+    ExperienciaLaboralComponent,
+    HardSoftSkillsComponent,
+    ProyectosComponent,
+    IdiomasComponent
   ],
   imports: [
     BrowserModule,
